efddb6ff037844035451241889955a933a1d91b5
OBohutskyi <bohutskyi.oleksandr@gmail.com>
2019-12-04 20:11:32 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# tpd_eng project
+
+## Prerequisites
+Please move all json files that are needed to be analyzed to input folder.
+Please don't remove output.csv file from output folder. It would be override once you run the project.
+
+## Running project
+Run `python run.py` to start processing files. You'll receive console messages about process status. It will write which file is processing now. After running the project, open output.csv file in output directory to see the results.
---
+++ run.py
@@ -0,0 +1,84 @@
+import json
+import os
+import csv
+
+
+def extract_values(source, key):
+    arr = []
+
+    def extract(json_object, temp, key):
+        if isinstance(json_object, dict):
+            for k, v in json_object.items():
+                if isinstance(v, (dict, list)):
+                    extract(v, temp, key)
+                elif k in key:
+                    temp.append(v)
+        elif isinstance(json_object, list):
+            for item in json_object:
+                extract(item, temp, key)
+        return temp
+
+    return extract(source, arr, key)
+
+
+def process_file(file_name, result):
+    print('started processing', file_name, 'file')
+    with open(file_name, "r") as readFile:
+        data = json.load(readFile)
+        source_data = extract_values(data, 'name,location')
+        # result = {}
+        # print('source_data', source_data)
+        segment = source_data[1]
+        element_path = source_data[1]
+        for x in range(len(source_data)):
+            b = source_data[x]
+            if b.startswith('Segment-'):
+                segment = b[8:]
+                element_path = source_data[x]
+            elif b.startswith(segment):
+                if x+2 >= len(source_data):
+                    break
+                if not (source_data[x + 1].startswith(segment) or (source_data[x + 2].startswith('Segment-'))):
+                    if source_data[x + 2].find('/') != -1:
+                        # print(element_path + '/' + b + ':' + source_data[x + 2])
+                        if element_path + '/' + b + ':' + source_data[x + 2] in result:
+                            # print('update')
+                            result.update({element_path + '/' + b + ':' + source_data[x + 2]: result[
+                                                                                                  element_path + '/' + b + ':' +
+                                                                                                  source_data[
+                                                                                                      x + 2]] + 1})
+                        else:
+                            result[element_path + '/' + b + ':' + source_data[x + 2]] = 1
+    print('ended processing', file_name, 'file')
+    return result
+
+
+path = 'input'
+versions = {}
+
+for r, d, f in os.walk(path):
+    for file in f:
+        file_path = os.path.join(r, file)
+        print('reading file:', file_path)
+        start_src_index = file_path.find('_RSX')
+        start_dest_index = file_path.find('_to_')
+        end_index = file_path.find('_Transaction')
+        a = file_path[start_src_index + 1:start_dest_index]
+        c = file_path[start_dest_index+4:end_index+16]
+        if a+':'+c in versions:
+            versions[a + ':' + c] = process_file(os.path.join(r, file), versions[a + ':' + c])
+        else:
+            versions[a+':'+c] = process_file(os.path.join(r, file), {})
+
+# print(versions)
+print('started to write output file')
+output = open('output/output.csv', 'w')
+output.write('Source,Destination,ElementPath,SourcePath,Occurrences\n')
+for item in versions:
+    for i in versions[item]:
+        result = item + ',' + i + ',' + json.dumps(versions[item][i]) + '\n'
+        result = result.replace(':', ',')
+        output.write(result)
+
+output.close()
+print('finished to write output file')
